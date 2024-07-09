f8a80db015caf47468c88bfb327ac95a300a51a8
alexanosov <alexanosov47@gmail.com>
2024-07-09 19:38:49 +0300

    Sampleclient works with one thread, trying to modify mutexes and loops to make it work for multiple

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -9,21 +9,22 @@
 #define LAST_TWO_BITS 0x03
 #define THIRTY_ONE_BITS 0x7FFFFFFF
 #define STAGE_SHIFT 62
-#define PROCESSED_SHIFT 31
-#define STAGE_BITS >> STAGE_SHIFT & LAST_TWO_BITS // todo might be wrong
-#define PROCESSED_BITS >> PROCESSED_SHIFT & THIRTY_ONE_BITS
-#define TOTAL_BITS & THIRTY_ONE_BITS
+#define TOTAL_SHIFT 31
+#define STAGE_BITS(x) ((x >> STAGE_SHIFT) & LAST_TWO_BITS)
+#define PROCESSED_BITS(x) (x & THIRTY_ONE_BITS)
+#define TOTAL_BITS(x) ((x >> TOTAL_SHIFT) & THIRTY_ONE_BITS)
+
 
 typedef std::vector<IntermediateVec*>* twoDVec;
-
-
 
 
 struct Job{
     const MapReduceClient* mapReduceClient;
     int multiThreadLevel;
     JobState state;
-    std::atomic<uint64_t>* jobStateCounter;
+    std::atomic<uint64_t> jobStateCounter;
+    std::atomic<int> sortCounter;
+    std::atomic<bool> joined;
     pthread_t* pThreads;
     const InputVec* inputVec;
     OutputVec* outputVec;
@@ -31,20 +32,11 @@ struct Job{
     twoDVec shuffledVec;
     Barrier* barrier;
     pthread_mutex_t* mutex;
-    std::atomic<bool>* joined;
-    std::atomic<int>* mapCounter;
-    std::atomic<int>* sortShuffleCounter;
-    std::atomic<int>* shuffledVecSizeCounter;
-    std::atomic<int>* reduceCounter;
 
     ~Job(){
       delete[] pThreads;
       delete barrier;
       pthread_mutex_destroy (mutex); //todo might need to handle better
-      delete mapCounter;
-      delete sortShuffleCounter;
-      delete shuffledVecSizeCounter;
-      delete reduceCounter;
     }
 };
 
@@ -63,91 +55,108 @@ struct JobContext{
 };
 
 
-bool pairCompareFunc(IntermediatePair a, IntermediatePair b){
-  return a.first < b.first;
+bool pairCompareFunc(IntermediatePair& a, IntermediatePair& b){
+  return *(a.first) < *(b.first);
 }
 
 
-bool keysAreEqual(K2* a, K2* b){
-  return !(a>b) && !(b>a);
+bool keysAreEqual(K2& a, K2& b){
+  return !(a<b) && !(b<a);
 }
 
 
 void reduce(ThreadContext* threadContext){
   Job* job = threadContext->job;
-  //job->state = REDUCE_STAGE;
-  std::atomic<int>* reduceCounter = job->reduceCounter;
   const MapReduceClient* client = job->mapReduceClient;
-  while(reduceCounter->load() < job->shuffledVecSizeCounter->load ()){
-    int oldVal = *reduceCounter++;
-    client->reduce (job->shuffledVec->at(oldVal), threadContext);
+
+  if (pthread_mutex_lock (threadContext->job->mutex) != 0){
+      std::cerr << "system error: Failed to lock mutex" << std::endl;
+      exit(1);
+  }
+  auto total = TOTAL_BITS(job->jobStateCounter.load());
+  auto processed = PROCESSED_BITS(job->jobStateCounter.load());
+  if (pthread_mutex_unlock (threadContext->job->mutex) != 0){
+      std::cerr << "system error: Failed to unlock mutex" << std::endl;
+      exit(1);
+  }
+
+  while(processed < total){
+      if (pthread_mutex_lock (threadContext->job->mutex) != 0){
+          std::cerr << "system error: Failed to lock mutex" << std::endl;
+          exit(1);
+      }
+      int oldVal = processed;
+      job->jobStateCounter.fetch_add(1);
+      processed = PROCESSED_BITS(job->jobStateCounter.load());
+      if (pthread_mutex_unlock (threadContext->job->mutex) != 0){
+          std::cerr << "system error: Failed to unlock mutex" << std::endl;
+          exit(1);
+      }
+      client->reduce (job->shuffledVec->at(oldVal), threadContext);
   }
 }
 
 
-void shuffle(ThreadContext* threadContext)
-{
-  twoDVec sortedVectors = threadContext->job->sortedVectors;
-  std::atomic<int> *sortShuffleCounter = threadContext->job->sortShuffleCounter;
-  //todo check if there is an empty vector!
-  while (sortShuffleCounter->load () > 0)
-  {
-    K2 *maxKey = nullptr;
-    int maxKeyVecIdx = -1;
-    //Find the biggest key currently available
-    for (int i = 0; i < sortShuffleCounter->load(); i++)
-    {
-      IntermediateVec *currentVec = sortedVectors->at (i);
+void shuffle(ThreadContext* threadContext) {
+    twoDVec sortedVectors = threadContext->job->sortedVectors;
+    auto job = threadContext->job;
+    auto total = TOTAL_BITS(job->jobStateCounter.load());
+    auto processed = PROCESSED_BITS(job->jobStateCounter.load());
+    bool createNewVec = true;
+    //todo check if there is an empty vector!
+    while (processed < total) { //todo notice counter increment
+        K2 *maxKey = nullptr;
+        int maxKeyVecIdx = -1;
+        //Find the biggest key currently available
+        for (int i = 0; i < job->sortCounter.load(); i++) {
+            IntermediateVec *currentVec = sortedVectors->at(i);
 
-      if (currentVec->empty()){
-        sortedVectors->erase(sortedVectors->begin() + i);
-        sortShuffleCounter--;
-        break;
-      }
-      if (maxKeyVecIdx == -1)
-      {
-        maxKeyVecIdx = i;
-        maxKey = currentVec->back().first;
-      }
-      else
-      {
-        if (maxKey < currentVec->back().first)
-        {
-          maxKeyVecIdx = i;
-          maxKey = currentVec->back().first;
+            if (currentVec->empty()) {
+                sortedVectors->erase(sortedVectors->begin() + i);
+                job->sortCounter.fetch_add(-1);
+                createNewVec = false;
+                break;
+            }
+            if (maxKeyVecIdx == -1) {
+                maxKeyVecIdx = i;
+                maxKey = currentVec->back().first;
+            } else {
+                if (maxKey < currentVec->back().first) {
+                    maxKeyVecIdx = i;
+                    maxKey = currentVec->back().first;
+                }
+            }
         }
-      }
+        if (createNewVec) {
+            //Create a vector that contains all the pairs with the max key
+            auto newVec = new IntermediateVec();
+            for (auto sortedVec: *sortedVectors) {
+                while (!sortedVec->empty() && keysAreEqual(*sortedVec->back().first, *maxKey)) {
+                    newVec->push_back(sortedVec->back());
+                    job->jobStateCounter.fetch_add(1);
+                    processed = job->jobStateCounter.load() & THIRTY_ONE_BITS;
+                    sortedVec->pop_back();
+                }
+            }
+            threadContext->job->shuffledVec->push_back(newVec);
+        }
+        createNewVec = true;
     }
-    //Create a vector that contains all the pairs with the max key
-    auto newVec = new IntermediateVec ();
-    for (auto sortedVec : *sortedVectors){
-      while(keysAreEqual (sortedVec->back().first, maxKey) &&
-      !sortedVec->empty()){
-        newVec->push_back (sortedVec->back());
-        sortedVec->pop_back();
-      }
-    }
-    threadContext->job->shuffledVec->push_back(newVec);
-    threadContext->job->shuffledVecSizeCounter++;
-  }
-  reduce(threadContext);
 }
 
 
 void sort(ThreadContext* threadContext){
-  //Sort
-  //todo check if there is an empty vector
   std::sort (threadContext->intermediateVec->begin (),
              threadContext->intermediateVec->end (), pairCompareFunc);
-  //Add the sorted vector to the shared pile
+
   if (pthread_mutex_lock(threadContext->job->mutex) != 0){
     std::cerr << "system error: Failed to lock mutex" << std::endl;
     exit(1);
   }
-  threadContext->job->sortedVectors->push_back
-  (threadContext->intermediateVec);
-  threadContext->job->sortShuffleCounter++;
-  threadContext->job->jobStateCounter++;
+
+  threadContext->job->sortedVectors->push_back(threadContext->intermediateVec);
+  threadContext->job->sortCounter.fetch_add(1);
+
   if (pthread_mutex_unlock(threadContext->job->mutex) != 0){
     std::cerr << "system error: Failed to unlock mutex" << std::endl;
     exit(1);
@@ -155,18 +164,46 @@ void sort(ThreadContext* threadContext){
 }
 
 
-void map(ThreadContext* threadContext){
-  Job* job = threadContext->job;
-  std::atomic<int>* mapStageCounter = job->mapCounter;
+void map(ThreadContext* threadContext) {
+    if (pthread_mutex_lock (threadContext->job->mutex) != 0){
+        std::cerr << "system error: Failed to lock mutex" << std::endl;
+        exit(1);
+    }
+    auto job = threadContext->job;
     const MapReduceClient* client = job->mapReduceClient;
+    auto total = TOTAL_BITS(job->jobStateCounter.load());
+    auto processed = PROCESSED_BITS(job->jobStateCounter.load());
+    if (pthread_mutex_unlock (threadContext->job->mutex) != 0){
+        std::cerr << "system error: Failed to unlock mutex" << std::endl;
+        exit(1);
+    }
 
-    while(mapStageCounter->load() < (int)job->inputVec->size()){ //todo size
-      // might be bad
-      int oldVal = *mapStageCounter++;
-      auto key = job->inputVec->at(oldVal).first;
-      auto val = job->inputVec->at(oldVal).second;
-      client->map (key,val, threadContext);
-      job->jobStateCounter++;
+    while (true) {
+        if (pthread_mutex_lock (threadContext->job->mutex) != 0){
+            std::cerr << "system error: Failed to lock mutex" << std::endl;
+            exit(1);
+        }
+        if (processed >= total){
+            if (pthread_mutex_unlock (threadContext->job->mutex) != 0){
+                std::cerr << "system error: Failed to unlock mutex" << std::endl;
+                exit(1);
+            }
+            break;
+        }
+        int oldVal = processed;
+        InputPair pair = job->inputVec->at(oldVal);
+        job->jobStateCounter.fetch_add(1);
+        if (pthread_mutex_unlock (threadContext->job->mutex) != 0){
+            std::cerr << "system error: Failed to unlock mutex" << std::endl;
+            exit(1);
+        }
+        client->map(pair.first, pair.second, threadContext);
+//
+//        int oldVal = job->jobStateCounter.fetch_add(1)& THIRTY_ONE_BITS;
+//        InputPair pair = job->inputVec->at(oldVal);
+//        client->map(pair.first, pair.second, threadContext);
+//
+//        processed = PROCESSED_BITS(job->jobStateCounter.load());
     }
     sort(threadContext);
 }
@@ -180,14 +217,13 @@ void updateJobState(ThreadContext* tc, uint32_t total, stage_t stage){
   tc->job->state.percentage = 0;
   tc->job->state.stage = stage;
   uint64_t newCounterVal = (static_cast<uint64_t>(stage) << STAGE_SHIFT) |
-      (static_cast<uint64_t>(0) << PROCESSED_SHIFT) | static_cast<uint64_t>
-      (total);
-  tc->job->jobStateCounter->store(newCounterVal);
+      (static_cast<uint64_t>(total) << TOTAL_SHIFT) | static_cast<uint64_t>
+      (0);
+  tc->job->jobStateCounter.store(newCounterVal);
   if (pthread_mutex_unlock (tc->job->mutex) != 0){
     std::cerr << "system error: Failed to unlock mutex" << std::endl;
     exit(1);
   }
-
 }
 
 
@@ -197,15 +233,21 @@ void updateJobState(ThreadContext* tc, uint32_t total, stage_t stage){
  */
 void* threadRoutine(void* tc){
   auto threadContext = (ThreadContext*)tc;
+  //Map and Sort stage
+  threadContext->job->barrier->barrier();
   if (threadContext->Id == 0){
     updateJobState(threadContext, threadContext->job->inputVec->size(),
                    MAP_STAGE);
   }
-  map(threadContext);
+  threadContext->job->barrier->barrier();
 
   threadContext->job->barrier->barrier();
+  map(threadContext);
+  threadContext->job->barrier->barrier();
+
+  //Shuffle stage
+  threadContext->job->barrier->barrier();
   if (threadContext->Id == 0){
-    //Find total of elements to shuffle
     uint32_t shuffleTotal = 0;
     for (int i = 0; i < (int)threadContext->job->sortedVectors->size(); i++){
       shuffleTotal += threadContext->job->sortedVectors->at(i)->size();
@@ -214,16 +256,17 @@ void* threadRoutine(void* tc){
     shuffle(threadContext);
   }
   threadContext->job->barrier->barrier();
-  uint32_t reduceTotal = 0;
-  for (int i = 0; i < (int)threadContext->job->shuffledVec->size(); i++){
-    reduceTotal += threadContext->job->shuffledVec->at(i)->size();
+
+  //Reduce stage
+  threadContext->job->barrier->barrier();
+  if (threadContext->Id == 0){
+      uint32_t reduceTotal = threadContext->job->shuffledVec->size();
+      updateJobState (threadContext, reduceTotal, REDUCE_STAGE);
   }
-  updateJobState (threadContext, reduceTotal, REDUCE_STAGE); //todo i think
-  // reduce total is the same as shuffle total
+  threadContext->job->barrier->barrier();
   reduce(threadContext);
 
   return (void*) threadContext;
-
 }
 
 
@@ -244,6 +287,9 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
   job->multiThreadLevel = multiThreadLevel;
   job->state = {UNDEFINED_STAGE, 0};
 
+  job->sortedVectors = new std::vector<IntermediateVec*>;
+  job->shuffledVec = new std::vector<IntermediateVec*>;
+  job->sortCounter.store(0);
 
   JobContext* jobContext = new JobContext;
   jobContext->state = UNDEFINED_STAGE;
@@ -260,15 +306,11 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
   }
   job->barrier = barrier;
   //Create Mutex
+  job->mutex = new pthread_mutex_t;
   if (pthread_mutex_init(job->mutex, nullptr) != 0){
       std::cerr << "system error: Failed to create new mutexes" << std::endl;
       exit(1);
     }
-  //Create atomic counters and find its max value
-  job->mapCounter = new std::atomic<int> (0);
-  job->sortShuffleCounter = new std::atomic<int> (0);
-  job->shuffledVecSizeCounter = new std::atomic<int> (0);
-  job->reduceCounter = new std::atomic<int> (0);
   job->joined = new std::atomic<bool>(false);
   //Create thread contexts
   jobContext->threadContexts = new ThreadContext[multiThreadLevel];
@@ -280,6 +322,7 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
   for (int i = 0; i < multiThreadLevel; i++){
     jobContext->threadContexts[i].Id = i;
     jobContext->threadContexts[i].job = job;
+    jobContext->threadContexts[i].intermediateVec = new IntermediateVec;
   }
   //Create pthreads
   job->pThreads = new pthread_t [multiThreadLevel];
@@ -291,8 +334,9 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
   for (int i = 0 ; i < multiThreadLevel; i++)
   {
     pthread_t *thread = job->pThreads + i;
+    ThreadContext* tc = jobContext->threadContexts + i;
     if (pthread_create (thread, nullptr, threadRoutine,
-                        thread))
+                        tc))
     {
       std::cout << "system error: Failed to create new pthread" << std::endl;
       exit (1);
@@ -308,8 +352,8 @@ void waitForJob(JobHandle job){
       std::cerr << "system error: Failed to lock mutex" << std::endl;
       exit(1);
     }
-    if(!jc->job->joined->load ()){
-      jc->job->joined->store(true);
+    if(!jc->job->joined.load ()){
+      jc->job->joined.store(true);
       for (int i = 0; i < jc->job->multiThreadLevel ; i++){
         if (pthread_join (*(jc->job->pThreads + i), nullptr) != 0){
           std::cerr << "system error: Failed to join thread" << std::endl;
@@ -326,11 +370,11 @@ void waitForJob(JobHandle job){
 
 void getJobState(JobHandle job, JobState* state){
   auto jc = (JobContext*) job;
-  auto counterVal = jc->job->jobStateCounter->load();
+  auto counterVal = jc->job->jobStateCounter.load();
   //Extract data via shifting the 64 bit unsigned long int
-  auto total = counterVal TOTAL_BITS;
-  auto processed = counterVal PROCESSED_BITS;
-  auto stage = counterVal STAGE_BITS;
+  auto total = TOTAL_BITS(counterVal);
+  auto processed = PROCESSED_BITS(counterVal);
+  auto stage = STAGE_BITS(counterVal);
   //Update the data in the argument
   state->stage = static_cast<stage_t>(stage);
   state->percentage = (float(processed) /(float) total) * 100;
@@ -345,24 +389,35 @@ void closeJobHandle(JobHandle job){
 
 
 void emit2 (K2* key, V2* value, void* context){
-  ((ThreadContext*) context)->intermediateVec->push_back (std::pair<K2*, V2*>
-      (key, value));
+    ThreadContext* tc = (ThreadContext*) context;
+    if (pthread_mutex_lock (tc->job->mutex) != 0){
+        std::cerr << "system error: Failed to lock mutex" << std::endl;
+        exit(1);
+    }
+
+    tc->intermediateVec->push_back (std::pair<K2*, V2*>(key, value));
+
+    if (pthread_mutex_unlock (tc->job->mutex) != 0){
+        std::cerr << "system error: Failed to unlock mutex" << std::endl;
+        exit(1);
+    }
 }
 
 
-void emit3 (K3* key, V3* value, void* context){
-  auto* tc = (ThreadContext*) context;
-  //Lock the mutex to access the shared output vector
-  if (pthread_mutex_lock(tc->job->mutex) != 0){
-    std::cerr << "system error: Failed to lock mutex" << std::endl;
-    exit(1);
-  }
+void emit3(K3* key, V3* value, void* context) {
+    auto* tc = (ThreadContext*) context;
+    // Lock the mutex to access the shared output vector
+    if (pthread_mutex_lock(tc->job->mutex) != 0){
+        std::cerr << "system error: Failed to lock mutex" << std::endl;
+        exit(1);
+    }
 
-  std::pair<K3*, V3*> newPair(key, value);
-  tc->job->outputVec->push_back (newPair);
+    std::pair<K3*, V3*> newPair(key, value);
+    tc->job->outputVec->push_back(newPair);
 
-  if (pthread_mutex_unlock(tc->job->mutex) != 0){
-    std::cerr << "system error: Failed to unlock mutex" << std::endl;
-    exit(1);
-  }
+    if (pthread_mutex_unlock(tc->job->mutex) != 0){
+        std::cerr << "system error: Failed to unlock mutex" << std::endl;
+        exit(1);
+    }
 }
+
